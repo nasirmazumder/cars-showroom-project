62c26130ca56af33db0a659b31d169d106eb9277
NASIR UDDIN MAZUMDER <79399783+nasirmazumder@users.noreply.github.com>
2021-09-28 17:38:59 +0300

    added rating start

--- src/Components/Header/ShowCars/ShowCars.js
+++ src/Components/Header/ShowCars/ShowCars.js
@@ -9,20 +9,26 @@ const ShowCars = (props) => {
         <div className='showCars'>
             <div >
                 <img src={img} alt="" />
+                
                 <h2>Name: {name}</h2>
                 <h4>Year: {year}</h4>
 
                 <p>Engine: {engine}</p>
                 <p>Option: {option}</p>
                 <p>Seat: {seater}</p>
-                <p>Rating: {rating}</p>
+                <p>Rating: {rating} <i class="fas fa-star filled"></i>
+                                <i class="fas fa-star filled"></i>
+                                <i class="fas fa-star filled"></i>
+                                <i class="fas fa-star empty"></i>
+                                <i class="fas fa-star empty"></i></p>
 
                 <h3>Price: ${price}</h3>
-                <button onClick={() => props.handleBuyNow(props.product)} className='btn-buyNow'>Buy Now</button>
+                <button onClick={() => props.handleBuyNow(props.car)} className='btn-buyNow'><i class="fas fa-car-side"></i>Buy Now<i class="far fa-handshake"></i></button>
 
 
 
             </div>
+
         </div>
     );
 };
