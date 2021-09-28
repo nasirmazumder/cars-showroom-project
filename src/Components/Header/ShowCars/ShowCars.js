3dd8faba5b363f5e2f8d27bfd50b7a07960ec47a
NASIR UDDIN MAZUMDER <79399783+nasirmazumder@users.noreply.github.com>
2021-09-28 18:14:31 +0300

    updated class name

--- src/Components/Header/ShowCars/ShowCars.js
+++ src/Components/Header/ShowCars/ShowCars.js
@@ -16,15 +16,14 @@ const ShowCars = (props) => {
                 <p>Engine: {engine}</p>
                 <p>Option: {option}</p>
                 <p>Seat: {seater}</p>
-                <p>Rating: {rating} <i class="fas fa-star filled"></i>
-                                <i class="fas fa-star filled"></i>
-                                <i class="fas fa-star filled"></i>
-                                <i class="fas fa-star empty"></i>
-                                <i class="fas fa-star empty"></i></p>
+                <p>Rating: {rating} <i className="fas fa-star filled"></i>
+                                <i className="fas fa-star filled"></i>
+                                <i className="fas fa-star filled"></i>
+                                <i className="fas fa-star empty"></i>
+                                <i className="fas fa-star empty"></i></p>
 
                 <h3>Price: ${price}</h3>
-                <button onClick={() => props.handleBuyNow(props.car)} className='btn-buyNow'><i class="fas fa-car-side"></i>Buy Now<i class="far fa-handshake"></i></button>
-
+                <button onClick={() => props.handleBuyNow(props.car)} className='btn-buyNow'><i className="fas fa-car-side"></i>Buy Now<i className="far fa-handshake"></i></button>
 
 
             </div>
