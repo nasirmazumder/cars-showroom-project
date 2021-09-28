4ba5cf1b7f23cb551c1326b4651147c946247c9c
NASIR UDDIN MAZUMDER <79399783+nasirmazumder@users.noreply.github.com>
2021-09-28 17:47:16 +0300

    added some text

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
                 <h1>AL SAFI CAR SHOWROOM AND CAR RENTALS</h1>
                 <br />
                 <h2 className="slogan">High-tech Performance at Low-Tech value</h2>
-
+                <h2>Minimum installment Up to 5000 Dolars</h2>
             </div>
 
         </div>
